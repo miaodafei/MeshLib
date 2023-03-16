b5ba66fd18d0b5f773e654fa7d1f663e0c8252a1
冯立宇 <fengliyu@fengliyudeMacBook-Pro.local>
2023-03-16 16:51:46 +0800

    test

--- examples/cmake-example/main.cpp
+++ examples/cmake-example/main.cpp
@@ -16,19 +16,23 @@ int main()
     MR::Mesh mesh2 = MR::MeshLoad::fromAnyStl(inFilePath2).value();
 
     // 平移
+    MR::Box3f box = mesh1.getBoundingBox();
+    float xDistance = MR::depth(box);
+    float zDistance = MR::height(box);
+    float yDistance = MR::width(box);
     MR::AffineXf3f xf3f, yf3f, zf3f;
     MR::Matrix3<float> A; // 直接用默认的矩阵就行
-    MR::Vector3<float> bx(10.0, 0.0, 0.0); // x轴正向平移10单位
-
-    MR::Vector3<float> by(0.0, 10.0, 0.0); // y轴正向平移10单位
-    MR::Vector3<float> bz(0.0, 0.0, 10.0); // z轴正向平移10单位
+    MR::Vector3<float> bx(xDistance, 0.0, 0.0); // x轴正向平移10单位
+    MR::Vector3<float> by(0.0, yDistance, 0.0); // y轴正向平移10单位
+    MR::Vector3<float> bz(0.0, 0.0, zDistance); // z轴正向平移10单位
 
     xf3f = MR::AffineXf3f( A, bx);
     yf3f = MR::AffineXf3f( A, by);
     zf3f = MR::AffineXf3f( A, bz);
-    //mesh1.transform(xf3f);
-    mesh1.transform(yf3f);
-    //mesh1.transform(zf3f);
+    
+    mesh1.transform(xf3f);
+    // mesh1.transform(yf3f);
+    // mesh1.transform(zf3f);
 
     // 将两个网格存储在同一个ply中
     mesh1.addPart(mesh2);
