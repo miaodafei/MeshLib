3604fd2ce3b19873d4dc27f35a51664aedcba3c8
冯立宇 <fengliyu@fengliyudeMacBook-Pro.local>
2023-03-16 18:07:47 +0800

    test

--- examples/cmake-example/main.cpp
+++ examples/cmake-example/main.cpp
@@ -9,8 +9,9 @@
 #include <iostream>
 #include "MRMesh/MRBox.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char const *argv[])
 {
+    if (argc < 3) return 0;
     // 加载stl文件
     std::filesystem::path inFilePath1 = argv[1];
     MR::Mesh mesh1 = MR::MeshLoad::fromAnyStl(inFilePath1).value();
@@ -32,12 +33,26 @@ int main(int argc, char* argv[])
     xf3f = MR::AffineXf3f( A, bx);
     yf3f = MR::AffineXf3f( A, by);
     zf3f = MR::AffineXf3f( A, bz);
-    
-    mesh1.transform(xf3f);
-    mesh1.transform(yf3f);
-    mesh1.transform(zf3f);
 
-    // 将两个网格存储在同一个ply中
+    int choice = argv[2];
+
+    switch (choice)
+    {
+    case 0:
+        mesh1.transform(xf3f);
+        break;
+    case 1:
+        mesh1.transform(yf3f);
+        break;
+    case 2:
+        mesh1.transform(zf3f);
+        break;
+    
+    default:
+        break;
+    }
+
+    // 合并mesh存储在ply中
     mesh1.addPart(mesh2);
     std::filesystem::path outFilePath = "transform_testz.ply";
     auto saveRes = MR::MeshSave::toPly(mesh1, outFilePath);
