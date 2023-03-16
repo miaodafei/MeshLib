2605f351a4aaf10342e8c0801b1de32d24e3ae18
冯立宇 <fengliyu@fengliyudeMacBook-Pro.local>
2023-03-16 15:42:35 +0800

    test

--- examples/cmake-example/main.cpp
+++ examples/cmake-example/main.cpp
@@ -9,12 +9,19 @@
 int main()
 {
     std::filesystem::path inFilePath1 = "mesh.stl";
-    MR::Mesh mesh1 = MR::MeshLoad::fromAnyStl(inFilePath);
+    MR::Mesh mesh1 = MR::MeshLoad::fromAnyStl(inFilePath).value();
     std::filesystem::path inFilePath2 = "mesh.stl";
-    MR::Mesh mesh2 = MR::MeshLoad::fromAnyStl(inFilePath);
+    MR::Mesh mesh2 = MR::MeshLoad::fromAnyStl(inFilePath).value();
 
-    MR::AffineXf3f xf3f = MR::AffineXf3f(3.0, 0);
-    mesh1.transform(xf3);
+    MR::AffineXf3f xf3f;
+    Vector3<float> a1(1.0, 0.0, 0.0);
+    Vector3<float> a2(0.0, 1.0, 0.0);
+    Vector3<float> a3(0.0, 0.0, 1.0);
+    Vector3<float> b(10.0, 0.0, 1.0);
+
+    xf3f = MR::AffineXf3f( a1, b);
+
+    mesh1.transform(xf3f);
 
     if (mesh1.has_value()) {
         std::filesystem::path outFilePath = "transform_test.ply";
