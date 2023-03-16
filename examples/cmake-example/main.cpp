8a1a325d14f3b64ba996f67ec4777f37eca82ac7
fengliyu <you@example.com>
2023-03-16 18:05:02 +0800

    test

--- examples/cmake-example/main.cpp
+++ examples/cmake-example/main.cpp
@@ -9,19 +9,20 @@
 #include <iostream>
 #include "MRMesh/MRBox.h"
 
-int main()
+int main(int argc, char* argv[])
 {
     // 加载stl文件
-    std::filesystem::path inFilePath1 = "mesh.stl";
+    std::filesystem::path inFilePath1 = argv[1];
     MR::Mesh mesh1 = MR::MeshLoad::fromAnyStl(inFilePath1).value();
-    std::filesystem::path inFilePath2 = "mesh.stl";
+    std::filesystem::path inFilePath2 = argv[1];
     MR::Mesh mesh2 = MR::MeshLoad::fromAnyStl(inFilePath2).value();
 
     // 平移
     MR::Box3f box = mesh1.getBoundingBox();
-    float xDistance = MR::depth(box);
-    float zDistance = MR::height(box);
-    float yDistance = MR::width(box);
+    float zDistance = MR::depth(box);
+    float yDistance = MR::height(box);
+    float xDistance = MR::width(box);
+    std::cout << " x:" << xDistance << " y: " << yDistance << " z: " << zDistance << std::endl;
     MR::AffineXf3f xf3f, yf3f, zf3f;
     MR::Matrix3<float> A; // 直接用默认的矩阵就行
     MR::Vector3<float> bx(xDistance, 0.0, 0.0); // x轴正向平移
@@ -33,12 +34,12 @@ int main()
     zf3f = MR::AffineXf3f( A, bz);
     
     mesh1.transform(xf3f);
-    // mesh1.transform(yf3f);
-    // mesh1.transform(zf3f);
+    mesh1.transform(yf3f);
+    mesh1.transform(zf3f);
 
     // 将两个网格存储在同一个ply中
     mesh1.addPart(mesh2);
-    std::filesystem::path outFilePath = "transform_testy.ply";
+    std::filesystem::path outFilePath = "transform_testz.ply";
     auto saveRes = MR::MeshSave::toPly(mesh1, outFilePath);
     return 0;
 }
